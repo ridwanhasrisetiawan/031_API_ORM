684b9a87fad5dcc5cecd224dad14dd34d6b5cfa3
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2026-07-20 15:27:55 +0700

    add Komik update endpoint

--- index.js
+++ index.js
@@ -40,3 +40,20 @@ app.get('/komik', async (req, res) => {
         res.send(err);
     }
 });
+
+app.put('/komik/:id', async (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+
+    try {
+        const komik = await db.Komik.findByPk(id);
+        if (!komik) {
+            return res.status(404).send({ message: 'Komik tidak ditemukan' });
+        }
+
+        await komik.update(data);
+        res.send({message: 'Komik berhasil diupdate', komik});
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
