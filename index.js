90d1315763272eefdc383c6e832ae3376534cdda
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2026-07-20 15:32:10 +0700

    add delete Komik route

--- index.js
+++ index.js
@@ -57,3 +57,18 @@ app.put('/komik/:id', async (req, res) => {
         res.status(500).send(err);
     }
 });
+
+app.delete('/komik/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const komik = await db.Komik.findByPk(id);
+        if (!komik) {
+            return res.status(404).send({ message: 'Komik tidak ditemukan' });
+        }
+
+        await komik.destroy();
+        res.send({ message: 'Komik berhasil dihapus' });
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
